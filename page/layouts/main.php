8520c3903321198f7e366823b6770901613f81f3
justin <mcjustin@umich.edu>
2010-09-14 21:31:34 -0400

    Version 2.03 - Added Google Analytics code, accessible now through justinjmc80@gmail.com

--- page/layouts/main.php
+++ page/layouts/main.php
@@ -41,6 +41,21 @@ $authenticated = 0; //authenticate($identifier);
 
 ?>
 
+<!-- Google Analytics Stuff -->
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-10096632-3']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
 </head>
 
 <body onload = "autoScroll()">
@@ -238,6 +253,7 @@ curl_close($ch);
    <div class = "bottombar"></div>
 </div>
 
+<!--  Janrain Login Stuff -->
  <script type="text/javascript">
   var rpxJsHost = (("https:" == document.location.protocol) ? "https://" : "http://static.");
   document.write(unescape("%3Cscript src='" + rpxJsHost +
